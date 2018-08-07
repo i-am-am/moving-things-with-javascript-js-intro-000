200220f36acd64bb462cb8937cd6479843f9d179
Alena <i.am.am.629@gmail.com>
2018-08-07 00:49:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.left.replace('px', '')
+  var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
 
-  if (right < window.width) {
-    dodger.style.left = `${right + 1}px`
+  if (dodger.style.right > 0) {
+    dodger.style.right = `${right - 1}px`
   }
 }
 
